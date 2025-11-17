3a7fc48c334f08a3923384a1396fe46ad5d0c702
kaio santos <kaiojugno@gmail.com>
2025-11-16 22:30:20 -0300

    added add and remove question method

--- src/main/java/component/Form.java
+++ src/main/java/component/Form.java
@@ -6,13 +6,19 @@ import java.util.ArrayList;
 
 public class Form {
     private String formTitle;
-    private ArrayList question;
+    private ArrayList<Question> question;
     private Instant date;
 
-    public Form(String formTitle, ArrayList<Question> questions){
+    public Form(String formTitle, Question firstQuestion){
         this.formTitle = formTitle;
-        this.question = questions;
+        this.question.add(firstQuestion);
         this.date = Instant.now();
     }
 
+    public void addQuestion(Question question){
+        this.question.add(question);
+    }
+    public void removeQuestion(Question question){
+        this.question.remove(question);
+    }
 }
